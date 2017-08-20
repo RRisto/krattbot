2aa826393a0acc769b293828ad3fa6da9f81aac4
RRisto <ristohinno@gmail.com>
2017-08-20 21:16:28 +0300

    added tf chatbot model

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,17 @@ import json
 from flask import Flask, request
 import apiai
 from config import Config
+from helpers import classify, model, response
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+# stemmer = LancasterStemmer()
+
+# things we need for Tensorflow
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
 
 # FB messenger credentials
 ACCESS_TOKEN=Config.ACCESS_TOKEN
@@ -14,8 +25,12 @@ ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 app = Flask(__name__)
 
+# create a data structure to hold user context
+
+
 @app.route('/', methods=['GET'])
 def verify():
+
     # our endpoint echos back the 'hub.challenge' value specified when we setup the webhook
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == 'foo':
@@ -27,8 +42,8 @@ def verify():
 def reply(user_id, msg):
     data = {
         "recipient": {"id": user_id},
-        # "message": {"text": msg}
-        "message": {"text": 'mulle meeldib vorstu'}
+        "message": {"text": msg}
+        # "message": {"text": 'mulle meeldib vorstu'}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
@@ -46,12 +61,14 @@ def handle_incoming_messages():
     req.query = message
 
     # get response from API.ai
-    api_response = req.getresponse()
-    responsestr = api_response.read().decode('utf-8')
-    response_obj = json.loads(responsestr)
-    if 'result' in response_obj:
-        response = response_obj["result"]["fulfillment"]["speech"]
-        reply(sender, response)
+    # api_response = req.getresponse()
+    api_response = response(message)
+    reply(sender, api_response)
+    # responsestr = api_response.read().decode('utf-8')
+    # response_obj = json.loads(responsestr)
+    # if 'result' in response_obj:
+    #     response = response_obj["result"]["fulfillment"]["speech"]
+    #     reply(sender, response)
 
     return "ok"
 
